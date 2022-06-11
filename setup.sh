7766d11674e8927e78b48f7f9d47ba76e353e48e
Masayuki Sunahara <tamanishi@gmail.com>
2022-06-11 12:48:15 +0900

    remove zprezto

--- setup.sh
+++ setup.sh
@@ -4,12 +4,6 @@ DOT_FILES=(
     .tmux.conf
     .vimrc
     .gitconfig
-    .zprezto/runcoms/zlogin
-    .zprezto/runcoms/zlogout
-    .zprezto/runcoms/zprofile
-    .zprezto/runcoms/zshenv
-    .zprezto/runcoms/zshrc
-    .zprezto/runcoms/zpreztorc
 )
 
 for file in ${DOT_FILES[@]}
